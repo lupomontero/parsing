707f6811399ca50e5a1d13cd7cf95f0e74ede3a0
Lupo Montero <lupomontero@gmail.com>
2020-04-13 16:06:29 -0500

    Updates CLI to use both example parsers

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,72 @@
 #! /usr/bin/env node
 
-const { expr } = require('..');
+const { expr } = require('../examples/parsing-arithmetic');
+const { json } = require('../examples/parsing-json');
 
-const main = (str) => {
-  const [result, remaining] = expr(str);
+const readInputFromStdin = () => new Promise((resolve, reject) => {
+  const chunks = [];
 
-  if (typeof result !== 'number') {
-    console.error('SyntaxError: Unexpected character around position 0');
-    process.exit(1);
+  process.stdin.setEncoding('utf8');
+
+  process.stdin.on('readable', () => {
+    let chunk;
+    while ((chunk = process.stdin.read()) !== null) {
+      chunks.push(chunk.trim());
+    }
+  });
+
+  process.stdin.on('end', () => resolve(chunks.join()));
+});
+
+class ParserError extends SyntaxError {
+  constructor(input, remaining) {
+    const pos = (
+      remaining && remaining.length
+        ? input.length - remaining.length
+        : 0
+    );
+    super(`Unexpected character around position ${pos}`);
+
+    this.input = input;
+    this.remaining = remaining;
+    this.pos = pos;
+  }
+  toString() {
+    const inputArr = [...this.input];
+    return `${this.constructor.name}: ${this.message}
+
+${this.input}
+${inputArr.reduce(
+  (memo, char, idx) => (idx === this.pos ? `${memo}^` : `${memo} `),
+  '',
+)}`;
+  }
+}
+
+const main = async (parserName, str) => {
+  if (!['arithmetic', 'json'].includes(parserName)) {
+    throw new Error(`Unknown parser: ${parserName}`);
+  }
+
+  const input = str || await readInputFromStdin();
+  const [result, remaining] = parserName === 'json' ? json(input) : expr(input);
+
+  if (typeof remaining !== 'string') {
+    throw new ParserError(input);
   }
 
   if (remaining.length) {
-    console.error(`SyntaxError: Unexpected character around position ${str.length - remaining.length}`);
-    process.exit(1);
+    throw new ParserError(input, remaining);
   }
 
-  console.log(result);
-  process.exit(0);
+  return result;
 };
 
 if (module === require.main) {
-  main(process.argv[2]);
+  main(process.argv[2], process.argv[3])
+    .then(result => console.dir(result, { depth: null }))
+    .catch((err) => {
+      console.error(`${err}`);
+      process.exit(1);
+    });
 }
