0f630e949daf5cd2b43ad42d29ab940d3b8ba963
Lupo Montero <lupomontero@gmail.com>
2020-04-12 22:26:35 -0500

    Adds example implementation of JSON parser

---
+++ json.js
@@ -0,0 +1,96 @@
+// Example JSON parser
+//
+// Grammar
+// * https://www.json.org/json-en.html
+// * https://www.crockford.com/mckeeman.html
+
+const { char, nat, int, some, seq, choice } = require('./lib/util');
+
+const ws = some(choice(char(''), char(' '), char('\t'), char('\n'), char('\r')));
+
+const nullVal = seq(
+  () => [char('n'), char('u'), char('l'), char('l')],
+  () => null,
+);
+
+const trueVal = seq(
+  () => [char('t'), char('r'), char('u'), char('e')],
+  () => true,
+);
+
+const falseVal = seq(
+  () => [char('f'), char('a'), char('l'), char('s'), char('e')],
+  () => false,
+);
+
+const boolVal = choice(trueVal, falseVal);
+
+const number = choice(int/*, fraction exponent*/);
+
+const character = choice(
+  // seq(char('\\'), escape),
+  (str) => {
+    const code = str.charCodeAt(0);
+    return (
+      code < 32 || code > 1114111 || ['"', '\\'].includes(str[0])
+        ? []
+        : [str[0], str.slice(1)]
+    );
+  },
+);
+
+const characters = some(character);
+
+const string = seq(
+  () => [char('"'), characters, char('"')],
+  ([_, s]) => s,
+);
+
+const element = seq(
+  () => [ws, value, ws],
+  ([_, v]) => v,
+);
+
+const elements = choice(
+  seq(() => [element, char(','), elements], ([x, _, y]) => [x, ...y]),
+  // NOTE: we match single element as sequence in order to get result as array.
+  seq(() => [element], v => v),
+);
+
+const array = choice(
+  seq(() => [char('['), ws, char(']')], () => []),
+  seq(() => [char('['), elements, char(']')], ([_, v]) => v),
+);
+
+const object = choice(
+  seq(() => [char('{'), ws, char('}')], () => ({})),
+  seq(() => [char('{'), members, char('}')], ([_, m]) => m),
+);
+
+const members = choice(
+  seq(() => [member, char(','), members], ([[k, v], _, o]) => ({ ...o, [k]: v })),
+  // NOTE: we match single member as sequence in order to get result as array.
+  seq(() => [member], ([[k, v]]) => ({ [k]: v })),
+);
+
+const member = seq(
+  () => [ws, string, ws, char(':'), element],
+  ([_, k, __, ___, v]) => [k, v],
+);
+
+const value = choice(nullVal, boolVal, number, string, array, object);
+
+module.exports = {
+  json: element,
+  element,
+  elements,
+  array,
+  number,
+  ws,
+  character,
+  characters,
+  string,
+  object,
+  members,
+  member,
+};
---
+++ json.spec.js
@@ -0,0 +1,127 @@
+const {
+  json,
+  element,
+  elements,
+  array,
+  number,
+  ws,
+  character,
+  characters,
+  string,
+  member,
+  members,
+  object,
+} = require('./json');
+
+describe('ws', () => {
+  it('should...', () => {
+    // console.log(ws(''));
+    expect(ws('  abc')).toEqual(['  ', 'abc']);
+    expect(ws(' \tabc')).toEqual([' \t', 'abc']);
+    expect(ws('\n\n\n  abc')).toEqual(['\n\n\n  ', 'abc']);
+  });
+});
+
+describe('number', () => {
+  it('should ...', () => {
+    expect(number('123')).toEqual([123, '']);
+    expect(number('123abc')).toEqual([123, 'abc']);
+    expect(number('abc')).toEqual([]);
+  });
+});
+
+describe('character', () => {
+  it('should ...', () => {
+    expect(character('123')).toEqual(['1', '23']);
+    expect(character('\\')).toEqual([]);
+    expect(character('"')).toEqual([]);
+    expect(character(String.fromCharCode(0))).toEqual([]);
+  });
+});
+
+describe('characters', () => {
+  it('should ...', () => {
+    expect(characters('123')).toEqual(['123', '']);
+    expect(characters('abc\\')).toEqual(['abc', '\\']);
+    expect(characters('\\')).toEqual(['', '\\']);
+  });
+});
+
+describe('string', () => {
+  it('should ...', () => {
+    expect(string('"123"')).toEqual(['123', '']);
+    expect(string('"abc"\\')).toEqual(['abc', '\\']);
+    expect(string('"\\"')).toEqual([]);
+  });
+});
+
+describe('element', () => {
+  it('should ...', () => {
+    expect(element('[1]')).toEqual([[1], '']);
+    // expect(number('123abc')).toEqual([123, 'abc']);
+    // expect(number('abc')).toEqual([]);
+  });
+});
+
+describe('elements', () => {
+  it('should ...', () => {
+    expect(elements('1,2,3')).toEqual([[1, 2, 3], '']);
+    expect(elements('1')).toEqual([[1], '']);
+  });
+});
+
+describe('array', () => {
+  it('should ...', () => {
+    expect(array('[1]')).toEqual([[1], '']);
+    expect(array('[1,2,3]')).toEqual([[1,2,3], '']);
+    expect(array('[ ]')).toEqual([[], '']);
+    expect(array('[ \n  ]')).toEqual([[], '']);
+  });
+});
+
+describe('member', () => {
+  it('should ...', () => {
+    expect(member('"foo": "bar"')).toEqual([['foo', 'bar'], '']);
+  });
+});
+
+describe('members', () => {
+  it('should ...', () => {
+    expect(members('"foo": "bar"')).toEqual([{ foo: 'bar' }, '']);
+    expect(members('"foo": "bar", "baz": true')).toEqual([{ foo: 'bar', baz: true }, '']);
+  });
+});
+
+describe('object', () => {
+  it('should ...', () => {
+    expect(object('{}')).toEqual([{}, '']);
+    expect(object('{ "foo": "bar" }')).toEqual([{ foo: 'bar' }, '']);
+    expect(object(`{
+      "foo": "bar",
+      "baz": true
+    }`)).toEqual([{ foo: 'bar', baz: true }, '']);
+  });
+});
+
+describe('json', () => {
+  it('should ...', () => {
+    expect(json('null')).toEqual([null, '']);
+    expect(json('true')).toEqual([true, '']);
+    expect(json('false')).toEqual([false, '']);
+    expect(json('123')).toEqual([123, '']);
+    expect(json('[123]')).toEqual([[123], '']);
+    expect(json('[true]')).toEqual([[true], '']);
+    expect(json('[1,2,false,[null,null,[3]]]')).toMatchSnapshot();
+    expect(json('[10,11,true,false,null]')).toMatchSnapshot();
+    expect(json('[1, true, ["oh my god!"]]')).toMatchSnapshot();
+    expect(json('{ "ok": true }')).toMatchSnapshot();
+    expect(json(`{
+      "ok": true,
+      "nested": {
+        "oh": "my",
+        "version": 2,
+        "tags": [1, 2, 3]
+      }
+    }`)).toMatchSnapshot();
+  });
+});
