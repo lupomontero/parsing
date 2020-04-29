b7e5c93722678cdb8f34030c94aef5a2e7a8f439
Lupo Montero <lupomontero@gmail.com>
2020-04-29 13:15:01 -0500

    Adds assertion to increase coverage

--- index.spec.js
+++ index.spec.js
@@ -60,6 +60,7 @@ describe('ws', () => {
     expect(ws('')).toEqual(['', '']);
     expect(ws('  abc')).toEqual(['  ', 'abc']);
     expect(ws(' \tabc')).toEqual([' \t', 'abc']);
+    expect(ws('\t\rabc')).toEqual(['\t\r', 'abc']);
     expect(ws('\n\n\n  abc')).toEqual(['\n\n\n  ', 'abc']);
   });
 });
